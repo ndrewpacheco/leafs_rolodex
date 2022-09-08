818597dbfd1ee0149a65bfb3ff7ee1cd81461293
Andrew  Pacheco <ndrewpacheco@gmail.com>
2022-09-08 15:36:11 -0400

    added default avatar jpg to app

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,22 +1,21 @@
-import React, { useState } from "react";
+import avatar from "../default_avatar.jpeg";
 
 const Avatar = ({ id, fullName }) => {
-  const defaultImgUrl =
-    "https://live.staticflickr.com/65535/52340120977_f154705256_m.jpg";
   const playerImgUrl = `https://cms.nhl.bamgrid.com/images/headshots/current/168x168/${id}.jpg`;
-
-  const [photo, setPhoto] = useState(playerImgUrl);
 
   // add default image if player image is broken
   const addDefaultSrc = (e) => {
-    setPhoto(defaultImgUrl);
     e.onerror = null;
-    e.target.src = defaultImgUrl;
+    e.target.src = avatar;
   };
 
   return (
     <div className='player--avatar'>
-      <img src={photo} alt={`${fullName}'s Avatar`} onError={addDefaultSrc} />
+      <img
+        src={playerImgUrl}
+        alt={`${fullName}'s Avatar`}
+        onError={addDefaultSrc}
+      />
     </div>
   );
 };
